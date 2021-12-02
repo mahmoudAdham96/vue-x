95519aa877c0980fae53b6f19fae5d80eb6cf611
mahmoudAdham96 <88621466+mahmoudAdham96@users.noreply.github.com>
2021-12-02 18:01:00 +0200

    vuex project

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <h3>{{ $store.state.count }}</h3>
     </div>
     <router-view />
   </div>
